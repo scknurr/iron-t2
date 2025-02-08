a52a2eea32c2daf47274eb5fac112bc6dee3e444
Steve <scknurr@gmail.com>
2025-02-08 17:55:31 -0600

    df

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css'; // Global CSS
+// Option A: Precompiled CSS
+import '@workday/canvas-kit-react/dist/index.css';
 
 // If you installed Workday CanvasKit via npm and want the CSS globally:
 // import '@workday/canvas-kit-css/dist/canvas-kit.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
