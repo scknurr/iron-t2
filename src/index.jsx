0758f92c612ed8a37fb7c578c70626e21d7448c3
Steve <scknurr@gmail.com>
2025-02-08 18:54:16 -0600

    sdfsd

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// index.ts
+import '@workday/canvas-tokens-web/css/base/_variables.css';
+import '@workday/canvas-tokens-web/css/system/_variables.css';
+import '@workday/canvas-tokens-web/css/brand/_variables.css';
+
 // Import CanvasKit's SCSS for global styling:
-import '@workday/canvas-kit-react/core/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
