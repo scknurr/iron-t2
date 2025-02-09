e546a878c38fadd806edb3c7399386ef66f5e879
scknurr <scknurr@gmail.com>
2025-02-08 19:53:47 -0600

    dsf

--- main.jsx
+++ main.jsx
@@ -1,15 +1,10 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css'; // your global CSS
-import '@workday/canvas-kit-css/dist/canvas-kit.css';
+import './styles.css';
 
-
-// If you installed Workday Canvas kit via npm, optionally import the CSS here:
-// import '@workday/canvas-kit-css/dist/canvas-kit.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
